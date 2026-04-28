38510cb7cc60a3918c2df42966edf9e9c519221a
Adolphe51 <adolpheguere75@gmail.com>
2026-04-28 17:03:29 +0100

    nouveau

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FaceLoginController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Welcome');
 });
 
 Route::get('/dashboard', function () {
